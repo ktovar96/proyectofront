a68da4e2f4b7a67523b69c078a31417beb8f5d13
unknown <ktovar@ucol.mx>
2022-03-23 19:34:49 -0600

    Botones Guardar Producto y Guardar Cliente agregados

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 class main {
     constructor(){
+        //this.productos = new Map();
+        //this.venta = new Array(); //id : 1, cantidad, precio
         let btnadd = document.getElementById("btnadd");
+        let btnSaveClient = document.getElementById("btnSaveClient");
+        let btnSavePrduct = document.getElementById('btnSaveProduct');
         btnadd.addEventListener('click', this.add);
+        btnSaveClient.addEventListener('click', this.addClientes);
+        btnSavePrduct.addEventListener('click', this.addProductos);
     }
 
     add(){
@@ -15,6 +21,56 @@ class main {
         
         cel1.innerHTML =  selProducts.options[selProducts.selectedIndex].text;
         cel2.innerHTML =  inpQuantity.value;
+    }
+
+    addClientes(){
+        let nameClient = document.getElementById('inpCName').value;
+        let rfcClient = document.getElementById('inpCRFC').value;
+        let zipcodeClient = document.getElementById('inpCZipcode').value;
+        console.log(nameClient);
+        console.log(rfcClient);
+        console.log(zipcodeClient);
+        let data = {
+            name : nameClient,
+            rfc : rfcClient,
+            zipcode : zipcodeClient
+        }
+        console.log(data);
+
+        fetch('http://localhost:1339/api/client',{
+            method : 'POST',
+            body : JSON.stringify(data),
+            headers: {'Content-Type': 'application/json'}
+        })
+        .then (res => res.json())
+        .then (res => {
+            console.log(res);
+        })
+    }
+    
+    addProductos(){
+        let nameProduct = document.getElementById('inpPName').value;
+        let quantityProduct = document.getElementById('inpPQuant').value;
+        let costProduct = document.getElementById('InpPCost').value;
+        console.log(nameProduct);
+        console.log(quantityProduct);
+        console.log(costProduct);
+        let data = {
+            name : nameProduct,
+            quantity : quantityProduct,
+            cost : costProduct
+        }
+        console.log(data);
+
+        fetch('http://localhost:1339/api/product', {
+            method : 'POST',
+            body : JSON.stringify(data),
+            headers : {'Content-Type': 'application/json'}
+        })
+        .then (res => res.json())
+        .then (res => {
+            console.log(res);
+        })
     }
 }
 
